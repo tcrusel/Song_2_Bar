2b8de536eb8ebed077f4a1672190ed9c431586d5
MICKAEL_MALLINGER <mallingermickael@sfr.fr>
2025-06-27 11:43:11 +0200

    add router backEnd

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-import favouriteGroupActions from "./modules/favourite/fovouriteGroupAction/favouriteGroupActions";
-router.post("/api/favourite_music_group", favouriteGroupActions.add);
+// import favouriteGroupActions from "./modules/favourite/fovouriteGroupAction/favouriteGroupActions";
+// router.post("/api/favourite_music_group", favouriteGroupActions.add);
 
 export default router;

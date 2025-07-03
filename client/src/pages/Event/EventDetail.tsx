4a4f6a7febf44c039ef98bb8dcb330a76a19e23d
tcrusel <crusel.thomas@gmail.com>
2025-07-03 19:49:20 +0200

    fix naming for merging US-05

---
+++ client/src/pages/Event/EventDetail.tsx
@@ -0,0 +1,11 @@
+import Participate from "../../components/Participate/Participate";
+
+function EventDetail() {
+  return (
+    <>
+      <Participate userId={10} eventId={30} />
+    </>
+  );
+}
+
+export default EventDetail;

b319c0720d71f68f395016eb2fafe503b539acff
MICKAEL_MALLINGER <mallingermickael@sfr.fr>
2025-07-09 15:56:20 +0200

    test maj osef

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import BarPage from "./pages/BarPage/BarPage";
-import EventDetails from "./pages/Event/EventDetails";
-import Events from "./pages/Event/Events";
+import EventDetails from "./pages/Eventtest/EventDetails";
+import Events from "./pages/Eventtest/Events";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login";
 import MusicGroup from "./pages/MusicGroup/MusicGroup";

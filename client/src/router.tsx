d6b3d45b4ddddb6efaf75c50920c5b02bab8896a
tcrusel <crusel.thomas@gmail.com>
2025-07-09 10:41:15 +0200

    Temp rename /event to fix casing issue

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import BarPage from "./pages/BarPage/BarPage";
-import EventDetails from "./pages/Event/EventDetails";
-import Events from "./pages/Event/Events";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import MusicGroup from "./pages/MusicGroup/MusicGroup";
 import Register from "./pages/Register/Register";
+import EventDetails from "./pages/event-temp-rename/EventDetails";
+import Events from "./pages/event-temp-rename/Events";
 
 const router = createBrowserRouter([
   {

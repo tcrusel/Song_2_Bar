38e563b1717658b1a9468a400ef3230d35816032
MICKAEL_MALLINGER <mallingermickael@sfr.fr>
2025-07-10 11:43:35 +0200

    fix export default app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,6 @@
-import fs from "node:fs";
-import path from "node:path";
-import cors from "cors";
+// Load the express module to create a web application
 
 import express from "express";
-import type { ErrorRequestHandler } from "express";
 
 const app = express();
 
@@ -21,14 +18,13 @@ const app = express();
 // You should NOT do that: such code uses the `cors` module to allow all origins, which can pose security issues.
 // For this pedagogical template, the CORS code allows CLIENT_URL in development mode (when process.env.CLIENT_URL is defined).
 
+import cors from "cors";
+
 if (process.env.CLIENT_URL != null) {
   app.use(cors({ origin: [process.env.CLIENT_URL] }));
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.text());
-app.use(express.raw());
+// If you need to allow extra origins, you can add something like this:
 
 /*
 app.use(
@@ -55,11 +51,34 @@ app.use(
 // 4. `express.raw()`: Parses requests with raw binary data.
 
 // Uncomment one or more of these options depending on the format of the data sent by your client:
-// /* ************************************************************************* */
+
+app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.text());
+// app.use(express.raw());
+
+/* ************************************************************************* */
 
 // Import the API router
 import router from "./router";
+
+// Mount the API router under the "/api" endpoint
 app.use(router);
+
+/* ************************************************************************* */
+
+// Production-ready setup: What is it for?
+
+// The code includes sections to set up a production environment where the client and server are executed from the same processus.
+
+// What it's for:
+// - Serving client static files from the server, which is useful when building a single-page application with React.
+// - Redirecting unhandled requests (e.g., all requests not matching a defined API route) to the client's index.html. This allows the client to handle client-side routing.
+
+import fs from "node:fs";
+import path from "node:path";
+
+// Serve server resources
 
 const publicFolderPath = path.join(__dirname, "../../server/public");
 
@@ -81,10 +100,20 @@ if (fs.existsSync(clientBuildPath)) {
   });
 }
 
+/* ************************************************************************* */
+
+// Middleware for Error Logging
+// Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
+
+import type { ErrorRequestHandler } from "express";
+
+// Define a middleware function to log errors
 const logErrors: ErrorRequestHandler = (err, req, res, next) => {
   // Log the error to the console for debugging purposes
   console.error(err);
   console.error("on req:", req.method, req.path);
+
+  // Pass the error to the next middleware in the stack
   next(err);
 };
 

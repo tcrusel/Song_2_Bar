5eff7fa3e119f011dc31d8b0f06a1f8e1e11609e
PIERRE_WIART <PWIART89@gmail.com>
2025-07-10 11:39:48 +0200

    merge with branch 08

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import BarPage from "./pages/BarPage/BarPage";
-import EventDetails from "./pages/Event/EventDetails";
-import Events from "./pages/Event/Events";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import MusicGroup from "./pages/MusicGroup/MusicGroup";
+import EventDetails from "./pages/event/EventDetails";
+import Events from "./pages/event/Events";
 
 import Register from "./pages/Register/Register";
 

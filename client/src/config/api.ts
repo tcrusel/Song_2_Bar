417b2d32d59993210d2091a651dd0fb1713e5344
Bastien CERIANI <bastien.ceriani@gmail.com>
2025-10-18 12:06:32 +0200

    fix: undefined env variable while calling api

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,5 +1,5 @@
-const URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_URL_PROD
-  : import.meta.env.VITE_API_URL_DEV;
+const URL = import.meta.env.VITE_API_URL
+  // ? import.meta.env.VITE_API_URL_PROD
+  // : import.meta.env.VITE_API_URL_DEV;
 
 export { URL };
